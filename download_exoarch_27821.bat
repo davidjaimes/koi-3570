06c861f61e2bf83d67f94a4f42a71cb69d6eb874
davidjaimes <david@djaimes.com>
2019-08-13 09:35:35 -0700

    initial commit

---
+++ download_exoarch_27821.bat
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# If wget is not installed on your system,
+# please refer to http://irsa.ipac.caltech.edu/docs/batch_download_help.html.
+#
+# Windows users: the name of wget may have version number (ie: wget-1.10.2.exe)
+# Please rename it to wget in order to successfully run this script
+# Also the location of wget executable may need to be added to the PATH environment.
+#
+wget -O 'kplr005023948-2010355172524_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/281/81/kplr005023948-2010355172524_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2010355172524_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/298/15/kplr005023948-2010355172524_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2011073133259_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/415/00/kplr005023948-2011073133259_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2011073133259_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/431/79/kplr005023948-2011073133259_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2011177032512_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/314/41/kplr005023948-2011177032512_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2011177032512_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/331/15/kplr005023948-2011177032512_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2012004120508_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/482/00/kplr005023948-2012004120508_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2012004120508_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/498/41/kplr005023948-2012004120508_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2012088054726_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/514/78/kplr005023948-2012088054726_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2012088054726_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/531/47/kplr005023948-2012088054726_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2012179063303_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/548/10/kplr005023948-2012179063303_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2012179063303_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/564/78/kplr005023948-2012179063303_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2013011073258_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/614/95/kplr005023948-2013011073258_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2013011073258_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/631/43/kplr005023948-2013011073258_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2013098041711_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/647/84/kplr005023948-2013098041711_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2013098041711_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/664/60/kplr005023948-2013098041711_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2013131215648_llc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/681/29/kplr005023948-2013131215648_llc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2013131215648_llc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/698/04/kplr005023948-2013131215648_llc_lc.tbl' -a search_345998328.log
+wget -O 'kplr005023948-2012060035710_slc.fits' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/753/70/kplr005023948-2012060035710_slc.fits' -a search_345998328.log
+wget -O 'kplr005023948-2012060035710_slc_lc.tbl' 'http://exoplanetarchive.ipac.caltech.edu:80/data/ETSS//Kepler/005/756/03/kplr005023948-2012060035710_slc_lc.tbl' -a search_345998328.log
