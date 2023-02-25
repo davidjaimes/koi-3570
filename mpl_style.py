c07ef46f5fb8b0e1a8b9abbf6f53c079ab016e29
David Jaimes <dj@davidjaimes.com>
2023-02-25 11:17:40 -0800

    Update mpl_style.py

--- mpl_style.py
+++ mpl_style.py
@@ -50,3 +50,58 @@ lightbg = {
     'savefig.dpi': 300,
     'savefig.transparent': True,
 }
+
+greyfox = {
+    # Lines
+    'lines.linewidth': 2.5,
+
+    # Patches
+
+    # Images
+
+    # Font
+    'font.size': 18.0,
+
+    # Axes
+    'axes.facecolor': 'white',
+    'axes.edgecolor': 'C7',
+    'axes.linewidth': 1.0,
+    'axes.grid': True,
+    'axes.labelcolor': 'C7',
+    'axes.axisbelow': True,
+    'axes.titlepad': 20.0,
+    'axes.titlecolor': 'C7',
+
+    # X-Ticks
+    'xtick.labelsize': 12.0,
+    'xtick.major.size': 10,
+    'xtick.minor.size': 5,
+    'xtick.color': 'C7',
+    'xtick.top': True,
+    'xtick.minor.visible': True,
+
+    # Y-Ticks
+    'ytick.labelsize': 12.0,
+    'ytick.major.size': 10,
+    'ytick.minor.size': 5,
+    'ytick.color': 'C7',
+    'ytick.right': True,
+    'ytick.minor.visible': True,
+
+    # Grid
+    'grid.linestyle': 'dashed',
+    'grid.linewidth': 0.5,
+    'grid.alpha':   1.0,
+
+    # Legend
+    'legend.facecolor': 'lightgrey',
+    'legend.framealpha': 0.9,
+    'legend.fontsize': 12.0,
+
+    # Figure
+    'figure.figsize': (6, 4),
+
+    # Other
+    'savefig.dpi': 300,
+    'savefig.transparent': True,
+}
